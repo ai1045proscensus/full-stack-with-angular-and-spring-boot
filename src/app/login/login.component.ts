61a230120f653e8224d6cdcd40bbbfc2894df084
ai1045proscensus <agca.software@gmail.com>
2022-11-25 06:55:11 +0300

    *ngIf directive&hardcoded auth.4 username&pwd

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,10 +9,17 @@ export class LoginComponent implements OnInit {
 
   username="ai2045"
   pwd=""
+  errorMessage="invalid credentials"
+  invalidLogin=false
 
   handleLogin(): void{
-    console.log(this.username)
-    console.log(this.pwd)
+    // console.log(this.username)
+    // console.log(this.pwd)
+    if(this.username === "ai2045" && this.pwd === "dummy"){
+      this.invalidLogin = false
+    } else {
+      this.invalidLogin = true
+    }
   }
   
 
