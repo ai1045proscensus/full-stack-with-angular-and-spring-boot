4bfbf6a37fd979b31b67a5bcfaab54876b1951de
ai1045proscensus <agca.software@gmail.com>
2022-11-26 09:41:36 +0300

    routing is in play now... muss noch enabled werden

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
+// When we were generating the Angular Application using NG new.
+// One of the questions we were asked was,
+// "Do we need routing?" and we said,
+// "Yes, we need routing."
+// And that's where we got a specific file AppRoutingModule.
+
+// So this is the module which will handle all the routing for us.
+
+
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+// ziel: we want to go from LoginComponent to the WelcomeComponent.
+// here (routes) we can define all the routes that we want to use in this specific application:
+// first attribute: "Path": what is the path at which you would want to expose this? (url)
+// =>"welcome". and which component defines that? => zugehöriges WelcomeComponent
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'welcome', component: WelcomeComponent}
+];
+
+// TODO: hier weiter
+// Thing is there is a tag for router,
+
+// which you'd need to use to enable routing.
+
+// Lets go to "app.component.html."
+
+// That's the first component that is loaded.
+
+// Inside here, earlier we had commented something
+
+// called, "router-outlet."
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
